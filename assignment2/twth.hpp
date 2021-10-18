62320ef0d94c0eaed096e12ef4b96236c2b65b30
Devansh <devansh.kujha2003@gmail.com>
2021-10-18 09:12:02 +0530

    #2 ADT corrections

--- assignment2/twth.hpp
+++ assignment2/twth.hpp
@@ -2,13 +2,22 @@
 //! This file can be used as a header for the code files
 //! or could be directly pasted.
 
+//! Debugging Requirements ->
+//!     insert() into the three after 3 items
+//!     The function twthnode* get(int val) should return the first node in case of "equal values"
+//!     (Equal values are not allowed in the set ADT. Check for this boundary)
+//! Code Left to be Added ->
+//!     delete() from the two three tree
+//!     split() and repair() from the two three tree
+//!     unite() for two trees at a particular node
+
 #include <iostream>
 using namespace std;
 #define int long long int
 
 
 /// This represents node of the Two Three Tree
-/// Type field is for determining the type of node ( NULL, SINGLE, LEAF, TWONODE, THREENODE)
+/// Type field is for determining the type of node ( NULL, SINGLE, LEAF, TWONODE, THREENODE )
 struct twthnode
 {
     int type;
@@ -25,16 +34,18 @@ class twth
 {
     private:
     twthnode* root;
-    int h;
 
     public:
     twth();
     twth(int val);
     twth(twthnode* a);
     twthnode* get();
+    twthnode* get(int val);
     void insert(int x);
     void insert(twthnode* node);
     int remove();
+    void split(twthnode* node);
+    void repair(twthnode* node);
     void display();
 };
 
@@ -48,6 +59,14 @@ struct ret_insert_part
     int m;
 };
 
+/// Structure returned by the unite() function
+/// used as a function for making merge()
+/// later on.
+struct ret_unite
+{
+    twth* t1;
+    twth* t2;
+};
 
 /// This is a function which can be called directly on the root
 /// of two three tree and will free up all space occupied by that tree
@@ -82,7 +101,9 @@ twthnode* single(twth* t)
     twthnode* temp=new twthnode;
     temp->type=1;
     temp->d1=-1;         temp->d2=-1;
-    temp->parent=NULL;   temp->left=t->get();
+    temp->parent=NULL;   
+    temp->left=t->get();
+    (t->get())->parent=temp;
     temp->right=NULL;    temp->middle=NULL;
     return temp;
 }
@@ -108,32 +129,37 @@ twthnode* twonode(int val,twth* t1,twth* t2)
     temp->d1=val;   temp->d2=-1;
     temp->parent=NULL;
     temp->left=t1->get();
+    (t1->get())->parent=temp;
     temp->middle=t2->get();
+    (t2->get())->parent=temp;
     temp->right=NULL;
     return temp;
 }
 
 /// Creates a node with three children for the Two Three Tree
 /// O(1) time
-twthnode* threenode(int val1,int val2,twth*t1,twth*t2,twth*t3)
+twthnode* threenode(int val1,int val2,twth* t1,twth* t2,twth* t3)
 {
     twthnode* temp=new twthnode;
     temp->type=4;
     temp->d1=val1;   temp->d2=val2;
     temp->parent=NULL;
-    temp->left=(*t1).get();
-    temp->middle=(*t2).get();
-    temp->right=(*t3).get();
+    temp->left=t1->get();
+    (t1->get())->parent=temp;
+    temp->middle=t2->get();
+    (t2->get())->parent=temp;
+    temp->right=t3->get();
+    (t3->get())->parent=temp;
     return temp;
 }
 
 /// This function returns a triple (n1,n2,m) where
 /// n1 and n2 are two three trees and m is the minimum in
 /// two three tree n2.
-/// O(h(T)) time.
 ret_insert_part* insert_part(twth* t,int val)
 {
-    ret_insert_part* p;
+    ret_insert_part* p=new ret_insert_part;
+    
     if((t->get())->type==2) {
         int v=(t->get())->d1;
         if(val==v) {  p->n1=t;  p->n2=NULL;  p->m=-1; }
@@ -146,6 +172,7 @@ ret_insert_part* insert_part(twth* t,int val)
             p->m=val; p->n1=t; p->n2=&th;
         }
     }
+    
     else if((t->get())->type==3) {
         int a=(t->get())->d1;
         twth alpha((t->get())->left);
@@ -173,8 +200,11 @@ ret_insert_part* insert_part(twth* t,int val)
             }
         }
     }
+    
     else {
+        // p->n1=t; p->n2=NULL;
         int a=(t->get())->d1;
+        // return p;
         int b=(t->get())->d2;
         twth alpha((t->get())->left);
         twth beta((t->get())->middle);
@@ -216,6 +246,7 @@ ret_insert_part* insert_part(twth* t,int val)
             }
         }
     }
+    
     return p;
 }
 
@@ -229,6 +260,7 @@ void delete_part(twth* t,int val)
 /// O(N) time where N is the total number of nodes.
 void disp(twthnode* root)
 {
+    if(root->type==0) return;
     int flag=1;
     if(root->left!=NULL) { flag=0; disp(root->left); }
     if(root->middle!=NULL) { flag=0; disp(root->middle); }
@@ -237,36 +269,78 @@ void disp(twthnode* root)
     return;
 }
 
+/// Makes null two three tree.
+/// O(1) time
 twth::twth()
 {
     root=null();
     return;
 }
 
+/// Makes a two three tree with one element.
+/// O(1) time
 twth::twth(int val)
 {
     root=leaf(val);
     return;
 }
 
+/// Makes a two three tree with a particular node as root.
+/// O(1) time
 twth::twth(twthnode* a)
 {
     root=a;
     return;
 }
 
-/// Returns the Root of the Two Three Tree
+/// Returns the root node of the Two Three Tree.
 /// O(1) time
 twthnode* twth::get()
 {
     return root;
 }
 
+/// Returns the node with the particular value.
+/// NULL if no such node is present.
+/// O(h(T)) time
+twthnode* twth::get(int val)
+{
+    twthnode* ans=root;    
+    while(ans!=NULL || ans->type==0) {
+        if(ans->type==2) {
+            if(ans->d1==val) return ans;
+            else return NULL;
+        }
+        else if(ans->type==3) {
+            if(ans->d1>val) ans=ans->left;
+            else ans=ans->middle;
+        }
+        else {
+            if(ans->d1>val) ans=ans->left;
+            else if(ans->d2>val) ans=ans->middle;
+            else ans=ans->right;
+        }
+    }
+    if(ans==NULL || ans->type==0) return NULL;
+    else return ans;
+}
+
+void unite(twth* th1,twth* th2,twth* th)
+{
+    // Merge the given tree
+    // with the object tree and this is done
+    // following the cases.    
+}
+
+/// Insert a leaf node with given value in the tree.
+/// O(h(T)) time.
 void twth::insert(int val)
 {
     int p=root->type;
-    twthnode* temp=root;
-    if(p==0) root=leaf(val);
+    if(p==0) {
+        free(root);  // Clears un-necessary null() node
+        root=leaf(val);
+    }    
     else if(p==2) {
         if(val==root->d1) {}
         else if(val<root->d1) {
@@ -280,10 +354,9 @@ void twth::insert(int val)
     }
     else {
         ret_insert_part* ret=insert_part(this,val);
-        if((ret->n2)->get()==null()) root=(ret->n1)->get();
+        if((ret->n2)==NULL) root=(ret->n1)->get();
         else root=twonode(ret->m,ret->n1,ret->n2);
     }
-    free_space(temp);
     return;
 }
 
@@ -292,6 +365,8 @@ void twth::insert(twthnode* node)
     if(node->type==0) return;
     else if(node->type==1) insert(node->left);
     else if(node->type==2) insert(node->d1);
+    
+    // Handle these two cases in h(T) iterations.
     else if(node->type==3) {}
     else {}
     return;
@@ -299,7 +374,8 @@ void twth::insert(twthnode* node)
 
 int twth::remove()
 {
-
+    int ans=0;
+    return ans;
 }
 
 void twth::display()
@@ -307,4 +383,14 @@ void twth::display()
     disp(root);
     cout<<"\n";
     return;
+}
+
+void twth::split(twthnode* node)
+{
+
+}
+
+void twth::repair(twthnode* node)
+{
+
 }
