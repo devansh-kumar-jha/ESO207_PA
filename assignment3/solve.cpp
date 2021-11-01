fa651e82f211655f1b199706f0052aeedb829541
Devansh <devansh.kujha2003@gmail.com>
2021-11-02 02:48:32 +0530

    #3 strategy decoded

---
+++ assignment3/solve.cpp
@@ -0,0 +1,63 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define int long long int
+
+int dfs(vector<vector<int>>& adj,vector<bool>& visited,vector<int>& part,int i,int last)
+{
+    visited[i]=true;
+    // cerr<<"came"<<" ";
+    if(last==-1) part[i]=1;
+    else if(part[i]!=0) {
+        if(part[last]==part[i]) return 1;
+        else return 0;
+    }
+    else {
+        if(part[last]==1) part[i]=2;
+        else part[i]=1;
+    }
+    // cerr<<"checks ok"<<" ";
+
+    for(int j=0;j<adj[i].size();j++) {
+        if(adj[i][j]==i) continue;
+        int t=dfs(adj,visited,part,adj[i][j],i);
+        // cerr<<"recursive dfs"<<" ";
+        if(t==1) return 1;
+    }
+    
+    return 0;
+}
+
+int32_t main()
+{
+    int n;  cin>>n;
+    int m;  cin>>m;
+    vector<vector<int>> adj(n);
+    vector<bool> visited(n,false);
+    vector<int> part(n,0);
+
+    for(int i=0;i<m;i++) {
+        int a,b;  cin>>a>>b;
+        adj[a-1].push_back(b-1);
+        adj[b-1].push_back(a-1);
+    }
+    // cerr<<"Input done"<<" ";
+
+    int flag=dfs(adj,visited,part,0,-1);
+    
+    if(flag==1) cout<<"Not Bipartite";
+    else {
+        vector<int> v1;
+        vector<int> v2;
+        for(int i=0;i<n;i++) {
+            if(part[i]==1) v1.push_back(i+1);
+            else v2.push_back(i+1);
+        }
+
+        for(int i=0;i<v1.size();i++) cout<<v1[i]<<" ";
+        cout<<"\n";
+        for(int i=0;i<v2.size();i++) cout<<v2[i]<<" ";
+    }
+
+    cerr<<"Time:"<<1000*((double)clock())/(double)CLOCKS_PER_SEC<<"ms/n";
+    return 0;
+}
