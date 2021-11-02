ac5bdfb42798a1fa7010f5807c4b3f633b94297d
Devansh <devansh.kujha2003@gmail.com>
2021-11-02 14:20:22 +0530

    #3 algorithm corrections

--- assignment3/code.cpp
+++ assignment3/code.cpp
@@ -1,10 +1,53 @@
+//! First a linked list implementation
+//! is required to form a Adjacency List Representation
+//! for any graph G(V,E) after which we will implement the specific
+//! dfs algorithm within the graph for solving the problem.
+
 #include<bits/stdc++.h>
 using namespace std;
 #define int long long int
 
+struct vertex
+{
+    int data;
+    struct vertex* next;
+    struct vertex* prev;
+};
+
+class graph
+{
+    private:
+    struct vertex** adj;
+    int size;
+
+    public:
+    graph(int x);
+    void insert();
+};
+
+graph::graph(int x)
+{
+    size=x;
+    adj=(struct vertex**)malloc(x*sizeof(struct vertex*));
+    return;
+}
+
+struct vertex* SENT=NULL;
+
+struct vertex* make_vertex(int data)
+{
+    vertex* temp=new vertex;
+    temp->data=data;
+    temp->next=SENT;
+    return temp;
+}
 
 
 int32_t main()
 {
+    int n,m;  cin>>n>>m;
+    graph G(n);
+    G.insert();
 
+    return 0;
 }
--- assignment3/solve.cpp
+++ assignment3/solve.cpp
@@ -18,7 +18,8 @@ int dfs(vector<vector<int>>& adj,vector<bool>& visited,vector<int>& part,int i,i
     // cerr<<"checks ok"<<" ";
 
     for(int j=0;j<adj[i].size();j++) {
-        if(adj[i][j]==i) continue;
+        if(adj[i][j]==last) continue;
+        if(visited[adj[i][j]]==true) continue;
         int t=dfs(adj,visited,part,adj[i][j],i);
         // cerr<<"recursive dfs"<<" ";
         if(t==1) return 1;
@@ -42,21 +43,28 @@ int32_t main()
     }
     // cerr<<"Input done"<<" ";
 
-    int flag=dfs(adj,visited,part,0,-1);
-    
-    if(flag==1) cout<<"Not Bipartite";
-    else {
-        vector<int> v1;
-        vector<int> v2;
-        for(int i=0;i<n;i++) {
-            if(part[i]==1) v1.push_back(i+1);
-            else v2.push_back(i+1);
+    for(int i=0;i<n;i++) {
+        if(visited[i]==false) {
+            int flag=dfs(adj,visited,part,i,-1);
+            if(flag==1) {
+                cout<<"Not Bipartite"<<"\n";
+                cerr<<"Time:"<<1000*((double)clock())/(double)CLOCKS_PER_SEC<<"ms/n";
+                return 0;
+            }
         }
-
-        for(int i=0;i<v1.size();i++) cout<<v1[i]<<" ";
-        cout<<"\n";
-        for(int i=0;i<v2.size();i++) cout<<v2[i]<<" ";
     }
+    
+    vector<int> v1;
+    vector<int> v2;
+    for(int i=0;i<n;i++) {
+        if(part[i]==1) v1.push_back(i+1);
+        else v2.push_back(i+1);
+    }
+
+    for(int i=0;i<v1.size();i++) cout<<v1[i]<<" ";
+    cout<<"\n";
+    for(int i=0;i<v2.size();i++) cout<<v2[i]<<" ";
+    cout<<"\n";
 
     cerr<<"Time:"<<1000*((double)clock())/(double)CLOCKS_PER_SEC<<"ms/n";
     return 0;
